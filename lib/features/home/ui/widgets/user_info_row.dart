7fffb39057e4773e12f86a006b85d387749b1551
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2024-02-04 20:28:54 +0300

    Small refactoring

--- lib/features/home/ui/widgets/user_info_row.dart
+++ lib/features/home/ui/widgets/user_info_row.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_example/features/home/model/user.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class UserInfoRow extends StatelessWidget {
   final User user;
@@ -12,18 +11,10 @@ class UserInfoRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-        await SystemChannels.platform.invokeMethod<void>(
-          'HapticFeedback.vibrate',
-          'HapticFeedbackType.lightImpact',
-        );
-      },
-      child: ListTile(
-        leading: Image.network(user.picture.medium),
-        title: Text(user.name.fullName),
-        subtitle: Text(user.email),
-      ),
+    return ListTile(
+      leading: Image.network(user.picture.medium),
+      title: Text(user.name.fullName),
+      subtitle: Text(user.email),
     );
   }
 }
