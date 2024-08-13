62ee83e17b5cbb5810f491f47117079ff4e93e11
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2024-08-13 16:10:53 +0300

    Added BlocConsumer example

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -15,45 +15,55 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'BLoC Example',
-        ),
+    return BlocProvider<UsersBloc>(
+      create: (_) => UsersBloc(
+        usersRepository: di.get<UsersRepository>(),
       ),
-      body: BlocProvider<UsersBloc>(
-        create: (_) => UsersBloc(
-          usersRepository: di.get<UsersRepository>(),
-        ),
-        child: BlocListener<ProfileBloc, ProfileBlocState>(
-          listener: (context, state) {
-            context.read<UsersBloc>().add(UsersBlocEventRefresh());
-          },
-          child: BlocBuilder<UsersBloc, UsersBlocState>(
-            buildWhen: (prev, curr) {
-              if (prev is UsersBlocStateLoaded && curr is UsersBlocStateLoaded) {
-                return !const DeepCollectionEquality().equals(prev.users, curr.users);
-              }
+      child: BlocConsumer<ProfileBloc, ProfileBlocState>(
+        listener: (context, state) {
+          context.read<UsersBloc>().add(UsersBlocEventRefresh());
+        },
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Builder(
+                builder: (_) {
+                  String title = 'BLoC Example';
 
-              return true;
-            },
-            builder: (context, state) {
-              return switch (state) {
-                UsersBlocStateLoading _ => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                UsersBlocStateLoaded state => UsersList(
-                    users: state.users,
-                  ),
-                UsersBlocStateError state => Center(
-                    child: Text(
-                      state.error,
+                  if (state is ProfileBlocStateLoaded) {
+                    title = "${state.profile.firstName} ${state.profile.lastName}";
+                  }
+
+                  return Text(title);
+                },
+              ),
+            ),
+            body: BlocBuilder<UsersBloc, UsersBlocState>(
+              buildWhen: (prev, curr) {
+                if (prev is UsersBlocStateLoaded && curr is UsersBlocStateLoaded) {
+                  return !const DeepCollectionEquality().equals(prev.users, curr.users);
+                }
+
+                return true;
+              },
+              builder: (context, state) {
+                return switch (state) {
+                  UsersBlocStateLoading _ => const Center(
+                      child: CircularProgressIndicator(),
                     ),
-                  ),
-              };
-            },
-          ),
-        ),
+                  UsersBlocStateLoaded state => UsersList(
+                      users: state.users,
+                    ),
+                  UsersBlocStateError state => Center(
+                      child: Text(
+                        state.error,
+                      ),
+                    ),
+                };
+              },
+            ),
+          );
+        },
       ),
     );
   }
