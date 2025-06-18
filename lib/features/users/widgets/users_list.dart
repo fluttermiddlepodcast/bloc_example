e0d9fd0370b2b563e222c7fbe36e325069cf2fe8
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2025-06-18 21:29:58 +0300

    added custom_lint to CI

--- lib/features/users/widgets/users_list.dart
+++ lib/features/users/widgets/users_list.dart
@@ -2,7 +2,6 @@ import 'package:bloc_example/features/users/bloc/users_bloc.dart';
 import 'package:bloc_example/features/users/bloc/users_bloc_event.dart';
 import 'package:bloc_example/features/users/bloc/users_bloc_state.dart';
 import 'package:bloc_example/features/users/widgets/user_info_row.dart';
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
