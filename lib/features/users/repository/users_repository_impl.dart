c99e32a3670888c4fc2830e84c37a92705139eff
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2026-02-22 20:35:19 +0300

    some few improvements

--- lib/features/users/repository/users_repository_impl.dart
+++ lib/features/users/repository/users_repository_impl.dart
@@ -8,37 +8,37 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 // --- RECORDS ---
-// class UsersRepositoryImpl extends UsersRepository {
-//   final Dio dio;
+class UsersRepositoryImpl extends UsersRepository {
+  final Dio dio;
 
-//   UsersRepositoryImpl(this.dio);
+  UsersRepositoryImpl(this.dio);
 
-//   @override
-//   Future<(List<User>?, String?)> fetchUsers({
-//     int limit = 30,
-//     int page = 0,
-//   }) async {
-//     try {
-//       final res = await dio.get('?page=$page&results=$limit');
-//       if (res.statusCode == HttpStatus.ok) {
-//         final rawUsers = res.data['results'] as List<dynamic>;
-//         final users = rawUsers.map((rawUser) => User.fromJson(rawUser)).toList();
+  @override
+  Future<(List<User>?, String?)> fetchUsers({
+    int limit = 30,
+    int page = 0,
+  }) async {
+    try {
+      final res = await dio.get('?page=$page&results=$limit');
+      if (res.statusCode == HttpStatus.ok) {
+        final rawUsers = res.data['results'] as List<dynamic>;
+        final users = rawUsers.map((rawUser) => User.fromJson(rawUser)).toList();
 
-//         return (users, null);
-//       }
+        return (users, null);
+      }
 
-//       return (
-//         null,
-//         '''Network error:
-//              - status code: ${res.statusCode}
-//              - data: ${res.data}
-//           ''',
-//       );
-//     } catch (e) {
-//       return (null, 'Network error: $e');
-//     }
-//   }
-// }
+      return (
+        null,
+        '''Network error:
+             - status code: ${res.statusCode}
+             - data: ${res.data}
+          ''',
+      );
+    } catch (e) {
+      return (null, 'Network error: $e');
+    }
+  }
+}
 
 // --- EITHER (dartz) ---
 // class UsersRepositoryImpl extends UsersRepository {
